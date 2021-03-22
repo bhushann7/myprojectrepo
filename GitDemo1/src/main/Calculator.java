49c8b3e18f3c0b2b9fbf53d65f9e08c81639ff97
Bhushan B <bhushan.bamble@gmail.com>
2021-03-22 18:09:48 +0530

    Second commit

--- GitDemo1/src/main/Calculator.java
+++ GitDemo1/src/main/Calculator.java
@@ -4,7 +4,10 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Hello github");
-	
 	}
+	
+	public static void add(int num1,int num2){
+	      System.out.println(num1+num2);
+	  }
 
 }
