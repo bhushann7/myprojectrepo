362968e867b129f4e9182d1b417b364982be8e4c
Bhushan B <bhushan.bamble@gmail.com>
2021-03-23 10:54:37 +0530

    Added br1fn1

--- GitDemo1/src/main/Calculator.java
+++ GitDemo1/src/main/Calculator.java
@@ -38,4 +38,8 @@ public class Calculator {
 	public static void masterFn1(int num1){
 	      System.out.println(num1*num1*num1);
 	  }
+	
+	public static void br1Fn1(int num1){
+	      System.out.println(num1*num1*num1);
+	  }
 }
